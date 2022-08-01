390373adba01bd64d0a7ea46d90ca87441fe1f6a
Leonid Yatskevich <53173112+leonidya@users.noreply.github.com>
2022-08-01 15:19:52 +0000

    More elegant way

--- main.py
+++ main.py
@@ -2,25 +2,24 @@ from replit import clear
 #HINT: You can call clear() to clear the output in the console.
 from art import logo
 print(logo)
-list_of_the_bidders = []
+new_dict = {}
 Continue_of_the_action = True 
 while Continue_of_the_action:
-    new_dict = {}
     name = input("What is your name?")
     bid_amount = int(input("What's your bid? $"))
     other_biders = input("Are there any other bidders? 'yes' or 'no'").lower()
     clear()
-    new_dict["name"] = name
-    new_dict["bid_amount"] = bid_amount
-    list_of_the_bidders.append(new_dict)
+    new_dict[name] = bid_amount
     if other_biders == 'no':
         Continue_of_the_action = False
-biggest_amount = 0
+highest_amount = 0
 winner_name = ""
-for bidder in list_of_the_bidders:
-    if bidder["bid_amount"]>biggest_amount:
-        biggest_amount = bidder["bid_amount"]
-        winner_name = bidder["name"]
-print(f'The winner is {winner_name} with a bid of ${biggest_amount}')
+print(new_dict)
+for bidder in new_dict:
+    biggest_amount = new_dict[bidder]
+    if biggest_amount > highest_amount:
+        highest_amount = biggest_amount
+        winner_name = bidder
+print(f'The winner is {winner_name} with a bid of ${highest_amount}')
             
     
